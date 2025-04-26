074d6e68a41b1e2771ce0858829aafb2d2325b3d
JehanBugli <jehan.bugli@gwmail.gwu.edu>
2025-04-26 18:16:36 -0400

    Working streamlit implementation
    Reaching a working streamlit app implementation with test model artifacts

---
+++ Code/src/cli/run_app.py
@@ -0,0 +1,48 @@
+# %% Imports
+
+import os
+import subprocess
+import sys
+
+# %%
+
+def main():
+    """
+    Runs the streamlit app, abstracting the 'streamlit run' command.
+    """
+
+    # Construct the path to to the app execution script
+    # TODO: Argparse
+
+    app_path = os.path.join('src', 'app', 'app.py')
+
+    app_path = os.path.abspath(app_path)
+
+    if not os.path.exists(app_path):
+         print(f"Cannot find app.py at: {app_path}")
+         sys.exit(1)
+
+
+    # Construct and execute a subprocess command
+
+    command = ['streamlit', 'run', app_path]
+    print(f"Executing command: {' '.join(command)}")
+
+    try:
+        subprocess.run(command, check=True)
+
+    except FileNotFoundError:
+         print("'streamlit' command not found!")
+         sys.exit(1)
+
+    except subprocess.CalledProcessError as e:
+         print(f"Error running Streamlit: {e}")
+         sys.exit(e.returncode)
+
+    except KeyboardInterrupt:
+         print("\nStreamlit server stopped.")
+         sys.exit(0)
+
+
+if __name__ == '__main__':
+    main()
