2cb109d70e91a2e14bc34de62aa8641b868295d5
JehanBugli <jehan.bugli@gwmail.gwu.edu>
2025-04-30 14:27:45 -0400

    Adding more src module docs
    Adding src docs to README, removing unused main function from train.py

--- Code/src/training/train.py
+++ Code/src/training/train.py
@@ -2,6 +2,7 @@
 Train a TF-IDF + LinearSVC classifier to predict policy areas from bill text.
 Saves the model, vectorizer, and label-encoder for inference.
 
+LogisticRegression and MultinomialNB are removed but retained for reference.
 """
 
 # %% Imports
@@ -106,11 +107,11 @@ def train(
         #     'clf__C': [1.0],
         #     'clf__max_iter': [250],
         # },
-#         {
-#             **tf_idf_dict,
-#             'clf': [MultinomialNB()],
-#             'clf__alpha': [0.5],
-#         }
+        # {
+        #     **tf_idf_dict,
+        #     'clf': [MultinomialNB()],
+        #     'clf__alpha': [0.5],
+        # }
     ]
     print(f"\nParameter grid:\n{param_grid}")
 
@@ -216,54 +217,6 @@ def save_artifacts(save_folder, model, vectorizer, label_encoder):
     joblib.dump(label_encoder, os.path.join(save_folder,'label_encoder.joblib'))
 
 
-def main():
-
-    # Craft and parse arguments
-
-    parser = argparse.ArgumentParser(
-        description='Train and save a policy_area classifier'
-    )
-    parser.add_argument(
-        '--input', '-i', required=False,
-        type=str,
-        default='input_data.parquet',
-        help='Path to .parquet data'
-    )
-    parser.add_argument(
-        '--save_folder', '-s', required=False,
-        default='model_artifacts',
-        help='Folder to save .joblib files'
-    )
-    parser.add_argument(
-        '--test-size', type=float, default=0.2,
-        help='Fraction of data to hold out for testing'
-    )
-    parser.add_argument(
-        '--random-state', type=int, default=42,
-        help='Random seed for reproducibility'
-    )
-    args = parser.parse_args()
-
-
-    # Read the specified input data file
-
-    df = pd.read_parquet(args.input)
-    texts = df['bill_text'].fillna('').values
-    labels = df['policy_area'].fillna('').values
-
-
-    # Execute the training function and save results
-
-    model, vect, le = train(
-        texts, labels,
-        test_size=args.test_size,
-        random_state=args.random_state
-    )
-    save_artifacts(
-        args.save_folder,
-        model, vect, le
-    )
-
 if __name__ == '__main__':
-    main()
+    pass
 # %%
